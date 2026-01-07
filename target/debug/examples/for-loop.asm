7ccc2c4f133d86dd2cd37e4d34dfb8816b34bc6d
DavidMik08 <david.mikacic2008+github@gmail.com>
2026-01-07 23:43:55 +0100

    Made the for loop example better

--- target/debug/examples/for-loop.asm
+++ target/debug/examples/for-loop.asm
@@ -1,24 +1,28 @@
-; basically for (int i = 0; i<10; i++) {} but in DASM
-loop_init:
-  ;int i = 0;
-  add 0 0 r1
+; this is an example of an for loop in DASM
+; the c equivalent would be something like this:
+; int i;
+; for(i = 0; i<10; i = i+1) {}
+
+add 0 0 r1
 
 loop_start:
-  ; i < 10
+  ; compare r1 to 10
   sub r1 10 r0
+  ; set the address
   add_igf loop_end.Low 0 r3
   add_igf loop_end.Mid 0 r4
   add_igf loop_end.High 0 r5
+  ; branch to the end if r1 == 10
   biz 0 0 r0
 
-  ; i++
+  ; this is where you can put code that will run inside of the loop
+
+  ; increment r1 (i)
   add r1 1 r1
 
-  sub 0 0 r0
+  ; branch back to the start of the loop
+  add 0 0 r0
   add_igf loop_start.Low 0 r3
   add_igf loop_start.Mid 0 r4
   add_igf loop_start.High 0 r5
   biz 0 0 r0
-
-loop_end:
-  brk 0 0 r0
